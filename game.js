5f0d738e0483067f7e1864032ea3710e96da56e1
Kyle <adams.johnk@gmail.com>
2022-02-28 12:09:37 -0600

    RPS add playRound function

--- game.js
+++ game.js
@@ -1,8 +1,34 @@
 // game script
 
+const playerMove = "ROCK";
+const computerMove = computerPlay();
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay(){
     let moves = ["rock", "paper", "scissors"];
     return moves[Math.floor(Math.random()*moves.length)];
 }
 
-console.log(computerPlay());
+function playRound(playerMove, computerMove){
+    playerMove = playerMove.toLowerCase();
+    if (playerMove === computerMove){
+        return "It's a tie!";
+    }
+    else if (playerMove === "rock" && computerMove === "scissors" || playerMove === "paper" && computerMove === "rock" || playerMove === "scissors" && computerMove === "paper"){
+        playerScore++
+        return `You Win! ${playerMove} beats ${computerMove}`;
+    }
+    else {computerScore++;
+        return `You Lose, ${computerMove} beats ${playerMove}`};
+
+}
+
+function game(){
+    for (let i = 0; i < 5; i++) {
+        playRound(playerMove, computerMove);
+     } 
+     return `${playerScore} to ${computerScore}`
+}
+
+console.log(game());
